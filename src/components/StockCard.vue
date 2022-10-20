5a0b29c752867a6e6c6b76fc066adc317eb7a45b
Timothy <timcheng112@gmail.com>
2022-10-21 00:01:43 +0800

    Added table in stock screener

--- src/components/StockCard.vue
+++ src/components/StockCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="stock-card-box">
     <div class="stock-logo-box">
-      <img :src="stock.pic" alt="apple_logo" class="stock-logo" />
+      <img :src="stock.data.logo_url" alt="stock_logo" class="stock-logo" />
     </div>
     <div class="stock-info">
-      <h2>{{ stock.symbol }}</h2>
-      <h3>{{ stock.price }} USD</h3>
+      <h2>{{ stock.data.symbol }}</h2>
+      <h3>{{ stock.data.currentPrice }} USD</h3>
     </div>
   </div>
 </template>
@@ -23,14 +23,14 @@ export default {
   display: flex;
   flex-direction: row;
   margin-top: 2vh;
-  border-style: solid;
+  border: 1px solid #dddddd;
   padding: 1vh;
   border-radius: 12px;
 }
 
 .stock-logo-box {
   padding: 2vh;
-  margin-right: 2vh;
+  margin-right: 1vw;
 }
 
 .stock-logo {
--- src/views/StockScreener.vue
+++ src/views/StockScreener.vue
@@ -4,83 +4,134 @@
     <h1 class="stock-title">Stock Screener</h1>
     <div class="trending-stocks-box">
       <h2>Top Stocks</h2>
-      <h3>By Market Cap</h3>
+      <h3 class="subtitle">by market cap</h3>
       <div class="list-box">
-        <li v-for="stock in stocks" v-bind:key="stock.symbol">
+        <li v-for="stock in stocksInfo" v-bind:key="stock.symbol">
           <StockCard class="stock-item" v-bind:stock="stock" />
         </li>
       </div>
     </div>
-    <!-- <p>{{ getStock }}</p>
-    <div class="tradingview-widget-copyright">
-      <div class="Hello">
-        <Chart
-          :options="{
-            autosize: false,
-            theme: 'dark',
-            symbol: getStock,
-            style: 1,
-            allow_symbol_change: false,
-          }"
-        />
-        <TechnicalAnalysis :options="{ symbol: getStock }" />
-      </div>
-    </div> -->
+    <div>
+      <table>
+        <tr>
+          <th>Asset</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Market Cap</th>
+          <th>Recommendation</th>
+        </tr>
+        <tr v-for="stock in stocksInfo" v-bind:key="stock.data.symbol">
+          <td>{{ stock.data.symbol }}</td>
+          <td>{{ stock.data.shortName }}</td>
+          <td>${{ stock.data.currentPrice }}</td>
+          <td>${{ numberWithCommas(stock.data.marketCap) }}</td>
+          <td>{{ stock.data.recommendationKey }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-//import {src} from "https://s3.tradingview.com/tv.js"
-// import { Chart, TechnicalAnalysis } from "vue-tradingview-widgets";
 import StockCard from "../components/StockCard.vue";
+import axios from "axios";
 
 export default {
   name: "HelloWorld",
   components: {
     StockCard,
   },
-  //   mounted() {
-  //     if (localStorage.getItem("reloaded")) {
-  //       // The page was just reloaded. Clear the value from local storage
-  //       // so that it will reload the next time this page is visited.
-  //       localStorage.removeItem("reloaded");
-  //     } else {
-  //       // Set a flag so that we know not to reload the page twice.
-  //       localStorage.setItem("reloaded", "1");
-  //       localStorage.setItem("stock", this.$store.state.exchangeTicker);
-  //       location.reload();
-  //     }
-  //   },
-  //   computed: {
-  //     getStock() {
-  //       return localStorage.getItem("stock");
-  //     },
-  //   },
+
   data() {
     return {
       stocks: [
         {
           symbol: "AAPL",
-          price: "2.312T",
+          name: "Apple Inc",
+          price: "143.86",
           pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/976px-Apple_logo_black.svg.png?20220821121934",
+          market: "2.312T",
         },
         {
           symbol: "MSFT",
-          price: "1.764T",
+          name: "Microsoft Corp.",
+          price: "236.48",
           pic: "https://companieslogo.com/img/orig/MSFT-a203b22d.png?t=1633073277",
+          market: "1.764T",
         },
         {
           symbol: "GOOG",
-          price: "1.304T",
+          name: "Alphabet Inc (Google) Class C",
+          price: "100.29",
           pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png",
+          market: "1.304T",
         },
         {
           symbol: "AMZN",
-          price: "1.172T",
+          name: "Amazon.com, Inc",
+          price: "115.07",
           pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/2500px-Amazon_icon.svg.png",
+          market: "1.172T",
+        },
+        {
+          symbol: "AMZN",
+          name: "Amazon.com, Inc",
+          price: "115.07",
+          pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/2500px-Amazon_icon.svg.png",
+          market: "1.172T",
         },
       ],
+      stocksInfo: [],
     };
+  },
+
+  mounted() {
+    const symbols = [
+      "AAPL",
+      "MSFT",
+      "GOOG",
+      "AMZN",
+      "TSLA",
+      "BRK-B",
+      "JNJ",
+      "XOM",
+      "V",
+      "META",
+    ];
+    // let stocksInfo = [];
+
+    for (let i = 0; i < symbols.length; i++) {
+      const encodedParams = new URLSearchParams();
+      encodedParams.append("symbol", symbols[i]);
+
+      const options = {
+        method: "POST",
+        url: "https://yahoo-finance97.p.rapidapi.com/stock-info",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "X-RapidAPI-Key":
+            "9e5086a482msh9ed634f178722c5p160495jsna9d40bbc9f31",
+          "X-RapidAPI-Host": "yahoo-finance97.p.rapidapi.com",
+        },
+        data: encodedParams,
+      };
+
+      axios
+        .request(options)
+        .then((response) => {
+          this.stocksInfo.push(response.data);
+          console.log(this.stocksInfo[0].data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+  },
+
+  methods: {
+    numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
   },
 };
 </script>
@@ -88,27 +139,50 @@ export default {
 <style scoped>
 .stock-title {
   text-align: center;
+  padding-top: 2vh;
+}
+
+.subtitle {
+  opacity: 50%;
 }
 
 .trending-stocks-box {
-  width: 70%;
+  width: 90%;
   margin: auto;
   flex: auto;
   border-radius: 12px;
-  padding: 10px;
+  padding: 1vh;
 }
 
 .list-box {
   display: grid;
-  grid-template-columns: auto auto auto auto;
+  grid-template-columns: auto auto auto auto auto;
 }
 
 .stock-item {
-  margin-right: 1vh;
+  margin-right: 1vw;
 }
 
 li {
   list-style-type: none;
+}
+
+table {
+  border-collapse: collapse;
+  width: 90%;
+  margin: auto;
+  margin-bottom: 2vh;
+}
+
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 1vh;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
 }
 
 @media only screen and (max-width: 768px) {
