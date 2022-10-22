180007bbab4674e2d980d48cb5da8b214bcdbb79
LauWyeRock <97499447+LauWyeRock@users.noreply.github.com>
2022-10-22 17:35:11 +0800

    Update Wye Rock (#20)
    * Added StockCard component, StockScreener page
    
    * Added table in stock screener
    
    * Changed api call
    
    * Update StockScreener.vue
    
    * modified:   package-lock.json (#18)
    
    modified:   package.json
    deleted:    src/components/News2.vue
    modified:   src/components/NewsDetails.vue
    new file:   src/components/NewsPage.vue
    modified:   src/router/index.js
    modified:   yarn.lock
    
    * Added table for social sentiments (#19)
    
    Co-authored-by: Timothy <timcheng112@gmail.com>
    Co-authored-by: Audrey <77070720+Audrey-Goo@users.noreply.github.com>
    Co-authored-by: timcheng112 <77824545+timcheng112@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,96 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const router = createRouter({
-    history : createWebHistory(),
-    routes: [
-        {path : "/", component: () => import("../views/Home.vue"), name:"Home"},
-        {path : "/register", component: () => import("../views/Register.vue"), name:"Register"},
-        {path : "/sign-in", component: () => import("../views/SignIn.vue"), name:"SignIn"},
-        {path : "/feed", component: () => import("../views/Feed.vue"),name:"Feed",
-        
-            meta: {
-                requiresAuth:true,
-            }},
-        {path : "/news", component: () => import("../components/News2.vue"),name:"News"},
-        {path : "/forum", component: () => import("../views/Forum.vue"),name:"Forum"},
-        {path : "/forgotPassword", component: () => import("../views/ForgotPassword.vue"),name:"ForgotPassword"},
-        {path : "/stocks/", component: () => import("../views/StockPage.vue"),name:"Stocks"},
-        {path : "/chat", component: () => import("../views/Chat.vue"),name:"Chat"},
-        {path : "/createpost", component: () => import("../views/CreatePost.vue"),name:"CreatePost"},
-        {path : "/view-post", component: () => import("../views/ViewPost.vue"),name:"ViewPost"},
-        
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: () => import("../views/Home.vue"), name: "Home" },
+    {
+      path: "/register",
+      component: () => import("../views/Register.vue"),
+      name: "Register",
+    },
+    {
+      path: "/sign-in",
+      component: () => import("../views/SignIn.vue"),
+      name: "SignIn",
+    },
+    {
+      path: "/feed",
+      component: () => import("../views/Feed.vue"),
+      name: "Feed",
 
-    ]
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/news",
+      component: () => import("../components/NewsPage.vue"),
+      name: "News",
+    },
+    {
+      path: "/forum",
+      component: () => import("../views/Forum.vue"),
+      name: "Forum",
+    },
+    {
+      path: "/forgotPassword",
+      component: () => import("../views/ForgotPassword.vue"),
+      name: "ForgotPassword",
+    },
+    {
+      path: "/stocks/",
+      component: () => import("../views/StockPage.vue"),
+      name: "Stocks",
+    },
+    {
+      path: "/chat",
+      component: () => import("../views/Chat.vue"),
+      name: "Chat",
+    },
+    {
+      path: "/createpost",
+      component: () => import("../views/CreatePost.vue"),
+      name: "CreatePost",
+    },
+    {
+      path: "/view-post",
+      component: () => import("../views/ViewPost.vue"),
+      name: "ViewPost",
+    },
+    {
+      path: "/stock-screener",
+      component: () => import("../views/StockScreener.vue"),
+      name: "StockScreener",
+    },
+  ],
 });
 
 const getCurrentUser = () => {
-    return new Promise((resolve, reject) => {
-        const removeListener = onAuthStateChanged(
-            getAuth(),
-            (user) => {
-                removeListener();
-                resolve(user);
-            },
-            reject
-        )
-    })
-}
- 
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
+
 router.beforeEach(async (to, from, next) => {
-    if (to.matched.some((record) => record.meta.requiresAuth)) {
-        if (await getCurrentUser()) {
-            next();
-        } else {
-            alert("you dont have access");
-            next("/");
-        }
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next();
     } else {
-        next();
+      alert("you dont have access");
+      next("/");
     }
+  } else {
+    next();
+  }
 });
 
 export default router;
