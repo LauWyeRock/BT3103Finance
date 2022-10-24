056a21f2fba42d177db63dc255014a5bed474246
Audrey <77070720+Audrey-Goo@users.noreply.github.com>
2022-10-24 11:35:21 +0800

    Improved Design for Navigation Bar, News, Feed, Login, Chat (#21)
    * modified:   package-lock.json
    modified:   package.json
    deleted:    src/components/News2.vue
    modified:   src/components/NewsDetails.vue
    new file:   src/components/NewsPage.vue
    modified:   src/router/index.js
    modified:   yarn.lock
    
    * modified:   package-lock.json
    modified:   package.json
    modified:   src/components/NewsPage.vue
    modified:   yarn.lock
    
    * modified:   package-lock.json
    	modified:   src/components/Navigation.vue
    	modified:   src/components/NewsPage.vue
    	modified:   src/firebase/firebase.js
    	modified:   src/main.js
    	modified:   src/views/Chat.vue
    	modified:   src/views/Home.vue
    	modified:   src/views/SignIn.vue
    	modified:   src/views/StockPage.vue
    	modified:   yarn.lock
    
    * modified:   src/components/NewsPage.vue
    	modified:   src/views/Feed.vue

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,23 +1,44 @@
 <template>
 	<div class="container">
-		<SearchBar @searchTermChange="onSearchTermChange"></SearchBar>
+
+		<!-- header + search bar -->
+        <div class="header">education</div>
+		<div class="subheader">Not sure where to start?</div>
+		<div class="description"> TradeBros has carefully curated a list of investing educational videos below,
+			ranging from easy to difficult concepts for investors or traders of all levels.
+		</div>
+		<!-- <SearchBar @searchTermChange="onSearchTermChange"></SearchBar> -->
+
+		<!-- Intro -->
+		<div class="vidheader">An Introduction to the Stock Market</div>
+
+		<!-- Medium -->
+		<div class="vidheader">Cryptocurrencies and What They Do</div>
+
+		<!-- Difficult -->
+		<div class="vidheader">Solidifying your Investing Foundations</div>
+
+
 		<div class="row">
 			<VideoDetail :video="selectedVideo" />
 			<VideoList @videoSelect="onVideoSelect" :videos="videosList"></VideoList>
 		</div>
+
+
+
 	</div>
 </template>
 
 <script>
 import axios from 'axios';
-import SearchBar from '../components/SearchBar';
+// import SearchBar from '../components/SearchBar';
 import VideoList from '../components/VideoList';
 import VideoDetail from '../components/VideoDetail';
 const API_KEY = 'AIzaSyBUSaxahFCy25CYIrSkopFfEzk0g7rhWZQ';
 export default {
 	name: 'App',
 	components: {
-		SearchBar,
+		// SearchBar,
 		VideoList,
 		VideoDetail
 	},
@@ -47,3 +68,61 @@ export default {
 	}
 };
 </script>
+
+<style scoped>
+
+	.container {
+		background: linear-gradient(
+            180deg,
+            rgba(161, 195, 209, 0.75) 46.38%,
+            rgba(241, 114, 161, 0.5) 100%
+        );
+		border: 0;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		width: 100%;
+	}
+
+	.header {
+		font-family: serif;
+		font-size: 480%;
+		font-weight: bold;
+		text-align: center; 
+		padding-top:30px;
+		letter-spacing: -1px;
+	}
+
+	.subheader {
+		padding-top: 50px;
+		width: 100px;
+		text-align:left;
+		margin: auto;
+		width: 80%;
+		font-size: 30px;
+		letter-spacing: -1px;
+		font-weight: bold;
+	}
+
+	.description {
+		font-size: 20px;
+		width: 100px;
+		text-align:left;
+		margin: auto;
+		width: 80%;
+		letter-spacing: 0px;
+	}
+
+	.vidheader {
+		padding-top: 50px;
+		width: 100px;
+		text-align:left;
+		margin: auto;
+		width: 80%;
+		font-size: 30px;
+		letter-spacing: -1px;
+		font-weight: bold;
+		text-decoration: underline;
+	}
+
+</style>
