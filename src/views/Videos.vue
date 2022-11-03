9ec334171d48c679c5de8429168117bf2d635629
Audrey <77070720+Audrey-Goo@users.noreply.github.com>
2022-11-04 00:45:26 +0800

    improved design for nav/header and create post page (#32)
    * modified:   package-lock.json
    modified:   package.json
    deleted:    src/components/News2.vue
    modified:   src/components/NewsDetails.vue
    new file:   src/components/NewsPage.vue
    modified:   src/router/index.js
    modified:   yarn.lock
    
    * modified:   package-lock.json
    modified:   package.json
    modified:   src/components/NewsPage.vue
    modified:   yarn.lock
    
    * modified:   package-lock.json
    	modified:   src/components/Navigation.vue
    	modified:   src/components/NewsPage.vue
    	modified:   src/firebase/firebase.js
    	modified:   src/main.js
    	modified:   src/views/Chat.vue
    	modified:   src/views/Home.vue
    	modified:   src/views/SignIn.vue
    	modified:   src/views/StockPage.vue
    	modified:   yarn.lock
    
    * modified:   src/components/NewsPage.vue
    	modified:   src/views/Feed.vue
    
    * modified:   src/components/Navigation.vue
    	modified:   src/views/Home.vue
    
    * Added router link to Video Page in Navigation.vue
    
    * modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/VideoDetail.vue
    	modified:   src/components/VideoList.vue
    	modified:   src/components/VideoListItem.vue
    	modified:   src/main.js
    	modified:   src/views/Videos.vue
    	modified:   yarn.lock
    
    * added max vid results of 9
    
    * modified:   src/components/BlogCoverPreview.vue
    	modified:   src/components/VideoList.vue
    	modified:   src/views/CreatePost.vue
    	modified:   src/views/Videos.vue
    
    * modified:   src/views/CreatePost.vue
    
    * modified:   src/components/Navigation.vue
    
    * modified:   src/components/Navigation.vue

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -82,13 +82,7 @@ export default {
 </script>
 
 <style scoped>
-.hello {
-  background-image: repeating-linear-gradient(
-    rgba(240, 235, 244, 1), 
-    rgba(161, 195, 209, 0.75), 
-    rgba(241, 114, 161, 0.5)
-    );
-}
+
 	button {
 		font-size: 20px;
 		font-weight: bold;
@@ -99,11 +93,11 @@ export default {
 		/* text-transform: lowercase;  */
 	}
 	.container {
-		/* background-image: repeating-linear-gradient(
+		background-image: repeating-linear-gradient(
 		rgba(240, 235, 244, 1), 
 		rgba(161, 195, 209, 0.5), 
 		rgba(241, 114, 161, 0.5)
-		); */
+		);
 		border: 0;
 		height: 100%;
 		margin: 0;
