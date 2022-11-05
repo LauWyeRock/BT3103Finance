5dc49c0f72363e8377a826c8d49cb1a700e35585
Audrey-Goo <audreygxy@gmail.com>
2022-11-05 21:46:11 +0800

    modified:   Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -227,7 +227,7 @@ header {
   width: 70px;
   background-color: rgb(252, 248, 243);
   border-radius: 5px;
-  color: rgb(252, 248, 243);
+  color: rgb(192, 140, 205);
   font-weight: bold;
   padding: 0;
 
