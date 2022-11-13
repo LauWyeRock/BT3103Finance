939a2a05ddc56b4cd25ed3a6427d85227b200144
LauWyeRock <97499447+LauWyeRock@users.noreply.github.com>
2022-11-13 16:26:32 +0800

    Update

--- src/components/ProfitDisplay.vue
+++ src/components/ProfitDisplay.vue
@@ -1,142 +1,160 @@
 <template>
-    <h1 id="Current">Current Portfolio</h1>
-  
-    <table id="table" class="auto-index">
-      <tr>
-        <th>S.No</th>
-        <th>Coin</th>
-        <th>Ticker</th>
-        <th>Buy_Price</th>
-        <th>Buy_Quantity</th>
-        <th>Current_Price</th>
-        <th>Profit</th>
-        <th>Options</th>
-      </tr>
-    </table>
-    <br /><br />
-    <h2 id="totalProfit"> TP </h2>
-  </template>
-  
-  <script>
-  // import ccxt from "ccxt";
-import {db} from "../firebase/firebase"
-  import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
-  
-  export default {
-    mounted() {
-      async function display(user) {
-        let z = await getDocs(collection(db, String(user)));
-        let ind = 1;
-        var tp = 0;
-  
-        z.forEach((docs) => {
-          let yy = docs.data();
-          var table = document.getElementById("table");
-          var row = table.insertRow(ind);
-  
-          var coin = yy.Coin;
-          var price = yy.Buy_Price;
-          var quantity = yy.Buy_Quantity;
-          var ticker = yy.Ticker;
-  
-          var cell1 = row.insertCell(0);
-          var cell2 = row.insertCell(1);
-          var cell3 = row.insertCell(2);
-          var cell4 = row.insertCell(3);
-          var cell5 = row.insertCell(4);
-          var cell6 = row.insertCell(5);
-          var cell7 = row.insertCell(6);
-          var cell8 = row.insertCell(7);
-  
-          cell1.innerHTML = ind;
-          cell2.innerHTML = coin;
-          cell3.innerHTML = ticker;
-          cell4.innerHTML = price;
-          cell5.innerHTML = quantity;
-          cell6.innerHTML = 0;
-          cell7.innerHTML = 0;
-  
-          cell7.className = "profits";
-  
-          var bu = document.createElement("button");
-          bu.className = "bwt";
-          bu.id = String(coin);
-          bu.innerHTML = "Delete";  
-          bu.onclick = function () {
-            deleteInstrument2(coin);
-          };
-          cell8.appendChild(bu);
-  
-          val(ticker);
-  
-          async function val(ticker) {
-            
-            // let binance = new ccxt.binance();
-            // let x = await binance.fetch_ohlcv(ticker, "5m");
-            let x = ticker; // this statement is bs
-            cell6.innerHTML = x[499][4];
-            cell7.innerHTML = Math.round(
-              quantity * (-parseFloat(price) + parseFloat(cell6.innerHTML))
-            );
-            tp = tp + parseFloat(cell7.innerHTML);
-            document.getElementById("totalProfit").innerHTML =
-              " Total Profit is " + String(tp);
-          }
-          ind += 1;
-        });
+  <h1 id="Current">Current Portfolio</h1>
+
+  <table id="table" class="auto-index">
+    <tr>
+      <th>S.No</th>
+      <th>Stock</th>
+      <th>Ticker</th>
+      <th>Buy_Price</th>
+      <th>Buy_Quantity</th>
+      <th>Current_Price</th>
+      <th>Profit</th>
+      <th>Options</th>
+    </tr>
+  </table>
+  <br /><br />
+  <h2 id="totalProfit">TP</h2>
+</template>
+
+<script>
+import { db } from "../firebase/firebase";
+import ccxt from "ccxt";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export default {
+  data() {
+    return {
+      user: false,
+      stockprice: "",
+      loading: false,
+    };
+  },
+
+  mounted() {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
       }
-      display();
-  
-      async function deleteInstrument2(coin) {
-        var x = coin;
-        alert("You are going to delete " + x);
-        await deleteDoc(doc(db, "Portfolio", x));
-        console.log("Document successfully created", x);
-        let tb = document.getElementById("table");
-        while (tb.rows.length > 1) {
-          tb.deleteRow(1);
+    });
+
+    async function display(user) {
+      let z = await getDocs(
+        collection(db, "stocks", String(user), "allStocks")
+      );
+      let ind = 1;
+      var tp = 0;
+
+      z.forEach((docs) => {
+        let yy = docs.data();
+        var table = document.getElementById("table");
+        var row = table.insertRow(ind);
+
+        var stock = yy.Stock;
+        var price = yy.Buy_Price;
+        var quantity = yy.Buy_Quantity;
+        var ticker = yy.Ticker;
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        var cell7 = row.insertCell(6);
+        var cell8 = row.insertCell(7);
+
+        cell1.innerHTML = ind;
+        cell2.innerHTML = stock;
+        cell3.innerHTML = ticker;
+        cell4.innerHTML = price;
+        cell5.innerHTML = quantity;
+        cell6.innerHTML = 0;
+        cell7.innerHTML = 0;
+
+        cell7.className = "profits";
+
+        var bu = document.createElement("button");
+        bu.className = "bwt";
+        bu.id = String(stock);
+        bu.innerHTML = "Delete";
+        bu.onclick = function () {
+          deleteInstrument2(stock);
+        };
+        cell8.appendChild(bu);
+
+        val(ticker);
+
+        async function val(ticker) {
+          let binance = new ccxt.binance();
+          let x = await binance.fetch_ohlcv(ticker, "5m");
+          cell6.innerHTML = x[499][4];
+          cell7.innerHTML = Math.round(
+            quantity * (-parseFloat(price) + parseFloat(cell6.innerHTML))
+          );
+          tp = tp + parseFloat(cell7.innerHTML);
+          document.getElementById("totalProfit").innerHTML =
+            " Total Profit is " + String(tp);
         }
-        document.getElementById("totalProfit").innerHTML = "";
-        display();
+        ind += 1;
+      });
+    }
+
+    display();
+
+    async function deleteInstrument2(stock) {
+      var x = stock;
+      alert("You are going to delete " + x);
+      await deleteDoc(doc(db, "Portfolio", x));
+      console.log("Document successfully created", x);
+      let tb = document.getElementById("table");
+      while (tb.rows.length > 1) {
+        tb.deleteRow(1);
       }
-    },
-  };
-  </script>
-  
-  <style scoped>
-  
-  h1,h2 {
-      text-align: center;
-      background-color: rgba(241, 114, 161, 0.5);
-      font: 700;
-      display: block;
-      font-size: 2em;
-      margin-block-start: 0.67em;
-      margin-block-end: 0.67em;
-      margin-inline-start: 0px;
-      margin-inline-end: 0px;
-      font-weight: bold;
-  }
-  
-  table{
-      font-family: arial, sans-serif;
-      border-collapse: collapse;
-      width:100%;
-  }
-  
-  tr:nth-child(even){
-      background-color: #e3edee;
-  }
-  
-  th,td {
-      border: 1px solid #dddddd;
-      text-align: center;
-      padding: 8px;
-  }
-  
-  .bwt{
-      color:rgb(243,236,236);
-      background-color: rgb(255,94,0);
-  }
-  
-  </style>
+      document.getElementById("totalProfit").innerHTML = "";
+      display();
+    }
+  },
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  text-align: center;
+  background-color: rgba(241, 114, 161, 0.5);
+  font: 700;
+  display: block;
+  font-size: 2em;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+tr:nth-child(even) {
+  background-color: #e3edee;
+}
+
+th,
+td {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+.bwt {
+  color: rgb(243, 236, 236);
+  background-color: rgb(255, 94, 0);
+}
+</style>
--- src/views/PaperTrading.vue
+++ src/views/PaperTrading.vue
@@ -8,7 +8,7 @@
 <script>
   import AddStock from '../components/AddStock.vue';
   import ProfitDisplay from '../components/ProfitDisplay.vue';
-  import axios from 'axios';
+  import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default {
   name: "App",
@@ -16,29 +16,24 @@ export default {
     AddStock,
     ProfitDisplay,
   },
+  mounted() {
+    const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            this.user = user;
+          }
+        })
+  },
   data() {
     return {
       refreshComp: 0,
       price: 0,
+      user: false,
       done: false,
       stockprice : "",
     };
   },
   methods: {
-    async stockPrice() {
-      this.url = 'https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=25531fecc6020f8bf9636c030e0e3b1a/' + this.stock
-      await axios.get(this.url)
-      .then( res => {
-        this.stockprice = res.data.price,
-        console.log(this.stockprice)
-        this.done = true
-      })
-      .catch( err => console.log(err))
-    },
-    formatNumber(number) {
-      number = (number/1000000).toFixed(2).replace('.',',')
-      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-    },
     change() {
       this.refreshComp += 1;
     },
@@ -56,113 +51,3 @@ export default {
 }
 
 </style>
-
-<!-- <template>
-  <div class="hello">
-  <h1>Showing Income Statement {{this.stock}}</h1>
-   <form @submit.prevent="updateStock">
-    <input type="submit" value="Submit" class="btn-primary m-2">
-    <input type="text" v-model="stock" name="stock" placeholder="Enter Ticker">
-   </form>
-    <div class="container">
-      {{this.stock}}
-      <div class="stock_name"> {{stockprice}}</div>
-    </div>
-
-    <div class="row" v-if="loaded">
-   
-
-    <div class="col-md-5 ml-5">
-      <b-table sticky-header hover :items="items" :fields="fields"></b-table>
-    </div>
-
-    <div class="col-md-5">
-      <chart :options="ChartOptionsBar"></chart>
-    </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import axios from 'axios'
-export default {
-  name: 'mainPage',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      url: '',
-      allfinancials: [],
-      loaded:'',
-      financials: [],
-      stock: '',
-      ChartOptionsBar: {},
-      stockprice: '',
-    }
-  },
- methods: {
-    // eslint-disable-next-line no-unused-vars
-    plotStock(allfinancials) {
-      
-      this.ChartOptionsBar = {
-        xAxis: {
-          data: [this.allfinancials[0].date,this.allfinancials[1].date, this.allfinancials[2].date, this.allfinancials[3].date ]
-        },
-        yAxis: {
-          type: 'value',
-        },
-        series: {
-          type: 'bar',
-          data: [this.allfinancials[0].Revenue/1000000, this.allfinancials[1].Revenue/1000000,this.allfinancials[2].Revenue/1000000,this.allfinancials[3].Revenue/1000000,]
-        }, 
-        title: {
-          text:'Yearly Revenues',
-          x: 'center',
-          textStyle: {
-            fontSize:24
-          }
-        },
-      }
-    },
-    async stockPrice() {
-      this.url = 'https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=25531fecc6020f8bf9636c030e0e3b1a/' + this.stock
-      await axios.get(this.url)
-      .then( res => {
-        this.stockprice = res.data.price,
-        console.log(this.stockprice)
-      })
-      .catch( err => console.log(err))
-    },
-    formatNumber(number) {
-      number = (number/1000000).toFixed(2).replace('.',',')
-      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-    },
-    async updateStock() {
-      this.url = 'https://financialmodelingprep.com/api/v3/income-statement/AAPL?apikey=25531fecc6020f8bf9636c030e0e3b1a/' + this.stock
-      console.log(this.url)
-      await axios.get(this.url)
-      .then( res => {
-        this.allfinancials = res.data.financials,
-        this.financials = res.data.financials[0];
-        this.loaded = true,
-       this.fields = ['item','in_Millions','in_Millions1'],
-        this.items = [
-         { in_Millions: this.allfinancials[0].date, item: Object.keys(this.financials)[0], in_Millions1:this.allfinancials[1].date, },
-        { in_Millions: this.formatNumber(this.allfinancials[0].Revenue) , item: Object.keys(this.financials)[1],in_Millions1: this.formatNumber(this.allfinancials[1].Revenue )},
-        { in_Millions: this.formatNumber(this.allfinancials[0]["Cost of Revenue"]), item: Object.keys(this.financials)[3], in_Millions1: this.formatNumber(this.allfinancials[1]["Cost of Revenue"])},
-        { in_Millions: this.formatNumber(this.allfinancials[0]["Operating Expenses"]), item: Object.keys(this.financials)[7], in_Millions1: this.formatNumber(this.allfinancials[1]["Operating Expenses"])},
-        { in_Millions: this.formatNumber(this.allfinancials[0]["Operating Income"]), item: Object.keys(this.financials)[8], in_Millions1: this.formatNumber(this.allfinancials[1]["Operating Income"])},
-        { in_Millions: this.formatNumber(this.allfinancials[0]["Interest Expense"]), item: Object.keys(this.financials)[9], in_Millions1: this.formatNumber(this.allfinancials[1]["Interest Expense"])},
-        { in_Millions: this.formatNumber(this.allfinancials[0]["Earnings before Tax"]), item: Object.keys(this.financials)[10], in_Millions1: this.formatNumber(this.allfinancials[1]["Earnings before Tax"])},
-        { in_Millions: this.formatNumber(this.allfinancials[0]["Income Tax Expense"]), item: Object.keys(this.financials)[11],in_Millions1: this.formatNumber(this.allfinancials[1]["Income Tax Expense"])},
-        { in_Millions: this.formatNumber(this.allfinancials[0]["Net Income"]), item: Object.keys(this.financials)[14], in_Millions1: this.formatNumber(this.allfinancials[1]["Net Income"])},
-         ]
-        this.plotStock(this.allfinancials)
-        this.stockPrice()
-      })
-      .catch (err => console.log(err))
-    }
-  }
-}
-</script> -->
