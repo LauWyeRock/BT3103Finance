0d83489a3c1cb4a1b88be251725948e78cbfb429
LauWyeRock <97499447+LauWyeRock@users.noreply.github.com>
2022-11-16 17:04:08 +0800

    Test examples

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -10,3 +10,14 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.text()).toMatch(msg)
   })
 })
+
+
+// test('the best flavor is grapefruit', () => {
+//   expect(bestLaCroixFlavor()).toBe('grapefruit');
+// })
+
+// test('map calls its argument with a non-null argument', () => {
+//   const mock = jest.fn();
+//   [1].map(x => mock(x));
+//   expect(mock).toHaveBeenCalledWith(expect.anything());
+// });
